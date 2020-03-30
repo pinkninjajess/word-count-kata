53b89b5f75cb53c500c64af33f9ac932e832b045
pinkninjajess <jessica.valenti1@gmail.com>
2020-03-30 11:58:06 -0400

    Added text to be printed out to the user - to match requirements

--- src/main/java/infrastructure/ConsoleUserInterface.java
+++ src/main/java/infrastructure/ConsoleUserInterface.java
@@ -7,13 +7,14 @@ import java.util.Scanner;
 public class ConsoleUserInterface implements UserInterface {
     @Override
     public String getUserInput() {
+        System.out.print("Enter text: ");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 
     @Override
     public void print(int count) {
-        System.out.println(count);
+        System.out.println("Number of words: " + count);
     }
 
 
