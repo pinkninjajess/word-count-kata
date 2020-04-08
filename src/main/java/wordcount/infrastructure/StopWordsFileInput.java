fa67b39c0b5595e3e7c827ae53f4ddbf4e986ce8
pinkninjajess <jessica.valenti1@gmail.com>
2020-04-08 19:40:38 -0400

    Updated to use classloader which enables the use of a relative file path to be used

--- src/main/java/wordcount/infrastructure/StopWordsFileInput.java
+++ src/main/java/wordcount/infrastructure/StopWordsFileInput.java
@@ -5,22 +5,20 @@ import wordcount.domain.FileInput;
 
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class StopWordsFileInput implements FileInput {
-    private ArrayList<String> stopWords;
     private static final Logger LOGGER = Logger.getLogger(StopWordsFileInput.class.getName());
     private String filePath;
 
     @Override
     public List<String> getWords() {
         try {
-            Class<StopWordsFileInput> stopWordsFileInputClass = StopWordsFileInput.class;
-            InputStream inputStream = stopWordsFileInputClass.getResourceAsStream(filePath);
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            InputStream inputStream = classLoader.getResourceAsStream(filePath);
             return IOUtils.readLines(inputStream, StandardCharsets.UTF_8);
         } catch (Exception e) {
             LOGGER.log(Level.WARNING, "Loading of stop words from file failed");
