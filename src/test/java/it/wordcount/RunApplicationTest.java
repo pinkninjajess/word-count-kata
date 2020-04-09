1ac74d33fb7707e6bc623b750f5b4dfc36cb26b8
pinkninjajess <jessica.valenti1@gmail.com>
2020-04-08 21:29:59 -0400

    Added integration tests to test RunApplication main method using fake System In values

---
+++ src/test/java/it/wordcount/RunApplicationTest.java
@@ -0,0 +1,63 @@
+package it.wordcount;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import wordcount.RunApplication;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class RunApplicationTest {
+    private final InputStream systemIn = System.in;
+    private final PrintStream systemOut = System.out;
+    private ByteArrayInputStream testIn;
+    private ByteArrayOutputStream testOut;
+
+    @Before
+    public void setUpOutput() {
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+    }
+
+    @After
+    public void restoreSystemInputOutput() {
+        System.setIn(systemIn);
+        System.setOut(systemOut);
+    }
+
+    @Test
+    public void runApplication_twoWordsProvided_SystemOutExpectedWithCorrectNumOfWords() {
+        String input = "hello world";
+        // fake user input into console;
+        provideInput(input);
+
+        RunApplication.main(new String[0]);
+
+        assertEquals("Enter text: Number of words: 2", getOutput());
+    }
+
+    @Test
+    public void runApplication_fileNameProvided_SystemOutExpectedWithCorrectNumOfWords() {
+        String input = "mytext.txt";
+        // fake user input into console;
+        provideInput(input);
+
+        RunApplication.main(new String[0]);
+
+        assertEquals("Enter text: Number of words: 4", getOutput());
+    }
+
+    private void provideInput(String data) {
+        testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    private String getOutput() {
+        return testOut.toString();
+    }
+}
